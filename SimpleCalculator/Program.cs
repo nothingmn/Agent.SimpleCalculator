d02d2fd7bc1ef2efee7f8ade9cbcd0f8c69fa4fe
Rob Chartier <rob.chartier@gmail.com>
2013-06-18 23:25:10 -0700

    initial import

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Presentation.Media;
+
+namespace SimpleCalculator
+{
+    public class Program
+    {
+        public static int AgentSize = 128;
+
+        public static void Main()
+        {
+            var screen = new Bitmap(Bitmap.MaxWidth, Bitmap.MaxHeight);
+
+            //new up our calculator
+            var calc = new Calculator();
+
+            //let it render itself
+            calc.Render(screen);
+
+
+            Debug.Print("Sleeping...");
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+    }
+}
